f4779d8ae7c8950a03c3ef62c52fb01b9867b504
hyezuu <duudu0704@gmail.com>
2024-07-09 02:55:10 +0900

    🚚 refactor: 프로젝트 이름을 server에서 sout으로 변경

--- server/src/main/java/com/sout/SoutApplication.java
+++ server/src/main/java/com/sout/SoutApplication.java
@@ -1,13 +1,13 @@
-package com.sout.server;
+package com.sout;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ServerApplication {
+public class SoutApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ServerApplication.class, args);
+        SpringApplication.run(SoutApplication.class, args);
     }
 
 }
--- server/src/test/java/com/sout/SoutApplicationTests.java
+++ server/src/test/java/com/sout/SoutApplicationTests.java
@@ -1,10 +1,10 @@
-package com.sout.server;
+package com.sout;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ServerApplicationTests {
+class SoutApplicationTests {
 
     @Test
     void contextLoads() {
