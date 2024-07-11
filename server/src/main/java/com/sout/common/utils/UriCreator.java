eaa6d7eff6d77866dc97bfc014f2843d2f29267a
hyezuu <duudu0704@gmail.com>
2024-07-12 06:09:27 +0900

    :recycle:refactor: createUri 예외처리 추가

--- server/src/main/java/com/sout/common/utils/UriCreator.java
+++ server/src/main/java/com/sout/common/utils/UriCreator.java
@@ -1,14 +1,18 @@
 package com.sout.common.utils;
 
+import com.sout.exception.InvalidUrlException;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
 
-public class UriCreator {
+public final class UriCreator {
     //유틸리티 클래스이므로 인스턴스화 할 수 없다.
     private UriCreator() {}
 
     public static URI createUri(String defaultUrl, long resourceId) {
+        if(defaultUrl==null || defaultUrl.isEmpty()){
+            throw new InvalidUrlException("defaultUrl is null or empty");
+        }
         return UriComponentsBuilder
                 .newInstance()
                 .path(defaultUrl + "/{resource-id}")
