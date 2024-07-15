eeec7d08ebc530b224eb27d3a4ed9ac667284081
hyezuu <duudu0704@gmail.com>
2024-07-15 13:40:01 +0900

    :sparkles: feat: Comment엔티티추가

--- server/src/main/java/com/sout/entity/Comment.java
+++ server/src/main/java/com/sout/entity/Comment.java
@@ -1,2 +1,39 @@
-package com.sout.entity.like;public class Comment {
+package com.sout.entity;
+
+import com.sout.common.base.BaseTimeEntity;
+import com.sout.entity.like.CommentLike;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@Entity(name = "comments")
+public class Comment extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id", nullable = false)
+    private Post post;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "parent_comment_id")
+    private Comment parentComment;
+
+    @OneToMany(mappedBy = "parentComment", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Comment> replies;
+
+    @OneToMany(mappedBy = "comment", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<CommentLike> likes;
 }
