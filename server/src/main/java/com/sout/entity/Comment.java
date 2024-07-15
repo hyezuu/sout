f38c374f56211653a26a01c6ac641a700ec7ceb5
hyezuu <duudu0704@gmail.com>
2024-07-15 10:43:40 +0900

    :sparkles: feat: post엔티티 추가

---
+++ server/src/main/java/com/sout/entity/Comment.java
@@ -0,0 +1,2 @@
+package com.sout.entity.like;public class Comment {
+}
---
+++ server/src/main/java/com/sout/entity/Post.java
@@ -0,0 +1,35 @@
+package com.sout.entity;
+
+import com.sout.common.base.BaseTimeEntity;
+import com.sout.entity.like.PostLike;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@Entity(name = "posts")
+public class Post extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String content;
+
+    @OneToMany(mappedBy = "post", cascade = CascadeType.REMOVE)
+    private List<PostImage> images = new ArrayList<>();
+
+    @OneToMany(mappedBy = "post", cascade = CascadeType.REMOVE)
+    private List<Comment> comments = new ArrayList<>();
+
+    @OneToMany(mappedBy = "post", cascade = CascadeType.REMOVE)
+    private List<PostLike> likes = new ArrayList<>();
+}
